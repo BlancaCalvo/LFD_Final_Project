ec1d09edf22228b2c294339f8a96db0a6cc9237a
ingalang <56589996+ingalang@users.noreply.github.com>
2019-10-24 13:24:33 +0200

    Update check_text_length_inga.py

--- check_text_length_inga.py
+++ check_text_length_inga.py
@@ -6,11 +6,20 @@ data = pd.read_csv('hyperp-training-grouped.csv.xz',
                    encoding='utf-8', index_col=0).dropna()
 
 empty_text_counter = 0
-
+# To count characters
 for row in data.itertuples(index=True, name='Pandas'):
     if len(getattr(row, "text")) < 70:
         print(getattr(row, "text"))
         print("NEXT")
         empty_text_counter += 1
+        
+        
+# To count words
+#for row in data.itertuples(index=True, name='Pandas'):
+#    words = getattr(row, "text").split(" ")
+#    if len(words) < 70:
+#        print(getattr(row, "text"))
+#        print("NEXT")
+#        empty_text_counter += 1
 
 print(empty_text_counter)
